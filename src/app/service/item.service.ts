cb15c56326396e5a4e07a4209563cf2eda5a1a69
shalitha <shalithaanuradha123@gmail.com>
2021-04-07 21:26:48 +0530

    After fully completed the backend

--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -10,22 +10,12 @@ export function getAllItems(): Promise<Array<Item>> {
 
     return new Promise((resolve, reject) => {
 
-
-
         let http = new XMLHttpRequest();
 
+        
         http.onreadystatechange = function () {
             if (http.readyState == 4) {
-                console.log("Hoory, Awa awa, items awa");
-                let dom = $(http.responseXML as any);
-                dom.find("item").each((index, elem) => {
-                    let code = $(elem).find("code").first().text();
-                    let description = $(elem).find("description").text();
-                    let qtyOnHand = $(elem).find("qty-on-hand").text();
-                    let unitPrice = $(elem).find("unit-price").text();
-                    items.push(new Item(code, description, +qtyOnHand, +unitPrice));
-                });
-                resolve(items);
+                resolve(items = JSON.parse(http.responseText));
             }
 
         }
