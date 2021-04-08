9498807c9c3b434c5d68d0a46bea807d08037294
shalitha <shalithaanuradha123@gmail.com>
2021-04-08 14:32:39 +0530

    After completing backend and front end for manage-customers except updating in front end

--- src/app/manage-customers/manage-customers.component.ts
+++ src/app/manage-customers/manage-customers.component.ts
@@ -9,12 +9,24 @@ import '../../../node_modules/admin-lte/plugins/datatables/jquery.dataTables.min
 import '../../../node_modules/admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js';
 import '../../../node_modules/admin-lte/plugins/datatables-responsive/js/dataTables.responsive.min.js';
 import '../../../node_modules/admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js';
-import { getAllCustomers, saveCustomer } from '../service/customer.service';
+import { deleteCustomer, getAllCustomers, saveCustomer } from '../service/customer.service';
 import { Customer } from '../model/customer';
 
 $("app-manage-customers").replaceWith('<div id="manage-customers">' + manageCustomers + '</div>');
 var html = '<style>' + style + '</style>';
 $("#dashboard").append(html);
+
+$("#tbl-customers tbody").on("click","tr .fas", async (event: Event)=>{
+    let id = $(event.target as any).parents("tr").find("td:first-child").text();
+    try{
+         await deleteCustomer(id);
+         alert("Customer has been deleted sucessfully")
+         loadAllCustomers()
+    }catch(error){
+        alert("Failed to delete the customer");
+    }
+   
+})
 
 let dataTable: any = null;
 
@@ -88,6 +100,8 @@ async function loadAllCustomers() {
         "ordering": false
     });
 
+    dataTable.page(Math.ceil(customers.length/5)-1).draw(false);
+
 }
 
 loadAllCustomers();
@@ -98,14 +112,23 @@ $("#btn-save").click(async () => {
     let name = <string>$("#txt-name").val();
     let address = <string>$("#txt-address").val();
 
-    let success = await saveCustomer(new Customer(id, name, address));
+    /* Front-end validation */
+    if (!id.match(/^C\d{3}$/) || name.trim().length==0 || address.trim().length==0){
+        alert("Invalid customer information");
+        return;
+    }
 
-    if (success) {
+    try{
+        await saveCustomer(new Customer(id, name, address));
         alert("Customer Saved");
         loadAllCustomers();
-    } else {
+    }catch(error){
         alert("Failed to save the customer");
+        // console.log(error);
     }
+
+
+    
 });
 
 
--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -3,7 +3,7 @@ import { Customer } from "../model/customer";
 
 // let customers: Customer[]=[];
 let customers: Array<Customer> = [];
-let loaded= false;
+let loaded = false;
 
 export function getAllCustomers(): Promise<Array<Customer>> {
     // To do : retrieve the data from the backend and fill the customers array.
@@ -11,47 +11,58 @@ export function getAllCustomers(): Promise<Array<Customer>> {
     return new Promise((resolve, reject) => {
 
         if (!loaded) {
-            //(1) Initiate the xml request
-            let http = new XMLHttpRequest();
+            // //(1) Initiate the xml request
+            // let http = new XMLHttpRequest();
 
-            // (2) Setting up the call back function
-            http.onreadystatechange = function () {
+            // // (2) Setting up the call back function
+            // http.onreadystatechange = function () {
 
-                if (http.readyState == 4) {
-                    // console.log("Hoory, Awa awa, customersla awa");
-                    // console.log(http.responseXML)  // console.log(http.responseXML)---->null
-                    // let dom = $(http.responseText);
+            //     if (http.readyState == 4) {
+            //         // console.log("Hoory, Awa awa, customersla awa");
+            //         // console.log(http.responseXML)  // console.log(http.responseXML)---->null
+            //         // let dom = $(http.responseText);
 
-                    // console.log(http.responseText)
-                    customers = JSON.parse(http.responseText);
-                    loaded=true;
-                    resolve(customers);
+            //         // console.log(http.responseText)
+            //         customers = JSON.parse(http.responseText);
+            //         loaded = true;
+            //         resolve(customers);
 
-                    // let dom = $(<any>http.responseXML);
-                    // dom.find("customer").each((index, elm) => {
-                    //     let id = $(elm).find("id").text();
-                    //     let name = $(elm).find("name").text();
-                    //     let address = $(elm).find("address").text();
-                    //     customers.push(new Customer(id, name, address));
-                    // });
-                    // resolve(customers);
+            //         // let dom = $(<any>http.responseXML);
+            //         // dom.find("customer").each((index, elm) => {
+            //         //     let id = $(elm).find("id").text();
+            //         //     let name = $(elm).find("name").text();
+            //         //     let address = $(elm).find("address").text();
+            //         //     customers.push(new Customer(id, name, address));
+            //         // });
+            //         // resolve(customers);
 
-                }
-            }
-
-            //(3) Let's open the request
-            http.open('GET', 'http://localhost:8080/pos/customers', true);
-
-            //(4) If we have to set headers
-
-            //(5) 
-            http.send();
-
-            // for(let i=0;i<50; i++){
-            //     // new Customer('C'+i,'Kasun','Galle');
-            //     customers.push(new Customer(`C${i}`,'Kasun','Galle'));
+            //     }
             // }
-        }else{
+
+            // //(3) Let's open the request
+            // http.open('GET', 'http://localhost:8080/pos/customers', true);
+
+            // //(4) If we have to set headers
+
+            // //(5) 
+            // http.send();
+
+            // // for(let i=0;i<50; i++){
+            // //     // new Customer('C'+i,'Kasun','Galle');
+            // //     customers.push(new Customer(`C${i}`,'Kasun','Galle'));
+            // // }
+
+            $.ajax({
+                method: "GET",
+                url: "http://localhost:8080/pos/customers"
+            }).then((data) => {
+                customers = data;
+                loaded = true;
+                resolve(customers);
+            }).fail(() => {
+                reject();
+            })
+        } else {
             resolve(customers);
         }
 
@@ -61,28 +72,72 @@ export function getAllCustomers(): Promise<Array<Customer>> {
 
 }
 
-export function saveCustomer(customer: Customer): Promise<boolean> {
+export function saveCustomer(customer: Customer): Promise<void> {
 
     return new Promise((resolve, reject) => {
-        let http = new XMLHttpRequest();
+        /*  let http = new XMLHttpRequest();
+ 
+         http.onreadystatechange = () => {
+             if (http.readyState == 4) {
+                 if (http.status == 201) {
+                     customers.push(customer);
+                     resolve();
+                 } else {
+                     // reject("Something went wrong");
+                 }
+             }
+         };
+ 
+         http.open('POST', 'http://localhost:8080/pos/customers', true);
+ 
+         http.setRequestHeader('Content-Type', 'application/json');
+ 
+         http.send(JSON.stringify(customer)); */
 
-        http.onreadystatechange = () => {
-            if (http.readyState == 4) {
-
-                let sucess = JSON.parse(http.responseText);
-                if (sucess) {
-                    customers.push(customer);
-                }
-                resolve(sucess);
-            }
-        };
-
-        http.open('POST', 'http://localhost:8080/pos/customers', true);
-
-        http.setRequestHeader('Content-Type', 'application/json');
-
-        http.send(JSON.stringify(customer));
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/pos/customers",
+            contentType: "application/json",
+            data: JSON.stringify(customer)
+        }).then(() => {
+            customers.push(customer);
+            resolve();
+        }).fail(()=>{
+            reject();
+        })
     });
 
 
+}
+
+export function deleteCustomer(id: string): Promise<void> {
+
+    return new Promise((resolve, reject) => {
+        // let http = new XMLHttpRequest();
+
+        // http.onreadystatechange = () => {
+        //     if (http.readyState == 4) {
+        //         if (http.status == 204) {
+        //             customers.splice(customers.findIndex((elm)=>elm.id===id), 1);
+        //             resolve();
+        //         } else {
+        //             // reject("Something went wrong");
+        //         }
+        //     }
+        // };
+
+        // http.open('DELETE', `http://localhost:8080/pos/customers?id=${id}`, true);
+
+        // http.send();
+
+        $.ajax({
+            method: "DELETE",
+            url: `http://localhost:8080/pos/customers?id=${id}`
+        }).then(() => {
+            customers.splice(customers.findIndex((elm) => elm.id === id), 1);
+            resolve();
+        }).fail(() => {
+            reject();
+        })
+    })
 }
